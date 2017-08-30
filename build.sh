e29430f54638b243b6bec4dcbc48a7c051403039
Anth0rx <anthorx@keemail.me>
2017-08-30 17:10:31 +0200

    Describe build.sh script

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
-#-------------------------#
-# Created 21. August 2017 #
-#-------------------------#
-
 #!/bin/bash
+
+#---------------------- INFO -------------------------------------------#
+#									#
+# commit: 	b2e46cd619c2589a99080aa12bb5f1209d24c61c		#
+# date:		21.08.17						#
+# author:	Anth0rx							#
+#									#
+#---------------------- DESCRIPTION ------------------------------------#
+#									#
+# This scipt is making a clean build of VegaStrike-taose.		#
+# After this is copies the relevant files to the 'bin' directory.	#
+#									#
+# The steps for creating this script were taken from the projects wiki:	#
+# https://github.com/Taose/Vegastrike-taose/wiki/How-to-Build		#
+#									#
+#-----------------------------------------------------------------------#
+
 
 ROOT_DIR=$(pwd)
 BUILD_DIR=$ROOT_DIR/engine/build
